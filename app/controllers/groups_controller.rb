ea76276cff91a0418685e429b16a4ad51cfb8110
chpwang <chpengwang@gmail.com>
2016-07-24 17:21:23 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
 
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 	before_action :find_group_and_check_auth, only: [:edit, :update, :destroy]
 
 	def index
